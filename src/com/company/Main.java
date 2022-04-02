9bfc0a05bf4e14c09f88970efc39fa1f9bfaaa51
Leiliane Loureço <leilianelourenzo@gmail.com>
2022-04-02 13:13:25 -0300

    Comite finala

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,6 @@
 package com.company;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
+
 import java.time.LocalDate;
 
 public class Main {
@@ -10,7 +10,7 @@ public class Main {
         Curso curso1 = new Curso();
         curso1.setTitulo("Curso Java");
         curso1.setDescricao("Descrição curso java");
-        curso1.setCargaHoraria(8);
+        curso1.setCargaHoraria(80);
 
         Curso curso2 = new Curso();
         curso2.setTitulo("Curso C#");
@@ -22,9 +22,38 @@ public class Main {
         mentoria1.setDescricao("Descrção mentoria de Java");
         mentoria1.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria1);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descricao Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devLeiliane = new Dev();
+        System.out.println("Leiliane");
+        devLeiliane.setNome("Leiliane");
+        devLeiliane.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devLeiliane.getCconteudosInscritos());
+        devLeiliane.progredir();
+        devLeiliane.progredir();
+        devLeiliane.progredir();
+        System.out.println(" - ");
+        System.out.println("Conteúdos Inscritos" + devLeiliane.getCconteudosInscritos());
+        System.out.println("Conteúdos Concluido" + devLeiliane.getCconteudosConcluidos());
+        System.out.println("XP: " + devLeiliane.calcularTotalXp());
+
+
+        Dev devMaria = new Dev();
+        System.out.println("Maria");
+        devMaria.setNome("Maria");
+        devMaria.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devMaria.getCconteudosInscritos());
+        devMaria.progredir();
+        System.out.println(" - ");
+        System.out.println("Conteúdos Inscritos" + devMaria.getCconteudosInscritos());
+        System.out.println("Conteúdos Concluidos" + devMaria.getCconteudosConcluidos());
+        System.out.println("XP: " + devMaria.calcularTotalXp());
+
 
     }
 }
